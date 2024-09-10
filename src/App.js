572ff4255f052203db7a55942daf5a683a64378f
WitAnas <160567253+WitAnas@users.noreply.github.com>
2024-09-10 11:34:32 +0530

    Add/Update src/App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import DynamicComponent from './DynamicComponent';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hello World</h1>
+      <DynamicComponent />
+    </div>
+  );
+}
+
+export default App;
+          
